d8746a4cf9d0af37dea931194892be9461059336
YanaB-b <j.biliuk96@gmail.com>
2023-06-11 19:21:07 +0200

    finish

---
+++ index.js
@@ -0,0 +1,38 @@
+const contacts = require("./contacts");
+const { Command } = require("commander");
+
+const program = new Command();
+program
+  .option("-a, --action <type>", "choose action")
+  .option("-i, --id <type>", "user id")
+  .option("-n, --name <type>", "user name")
+  .option("-e, --email <type>", "user email")
+  .option("-p, --phone <type>", "user phone");
+
+program.parse(process.argv);
+
+const argv = program.opts();
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const contactsList = await contacts.listContacts();
+      return console.log(contactsList);
+
+    case "get":
+        const oneContact = await contacts.getContactById(id);
+        return console.log(oneContact);
+      
+      case "add":
+        const newContact = await contacts.addContact(name, email, phone);
+        return console.log(newContact);
+  
+      case "remove":
+        const removeContact = await contacts.removeContact(id);
+        return console.log(removeContact);
+    }
+  
+    
+    }
+  
+  
+  invokeAction(argv);
